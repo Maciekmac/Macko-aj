eac8112c9d5ea0f47970356116acf46297519d23
Maciekmac <01206792@pw.edu.pl>
2025-12-18 17:29:12 +0100

    Update backsubst.c

--- lmp9/trunk/src/backsubst.c
+++ lmp9/trunk/src/backsubst.c
@@ -1,22 +1,35 @@
 #include "backsubst.h"
+#include <stdio.h>
+
 /**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
+ * Zwraca 0 - sukces
+ * Zwraca 1 - błąd dzielenia przez 0
+ * Zwraca 2 - błąd rozmiarów
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
+    int n = mat->r;
+    int i, j;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    
+    if (mat->r != mat->c || x->r != n || b->r != n) {
+        return 2;
+    }
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    
+    for (i = n - 1; i >= 0; i--) {
+        double sum = 0.0;
+        
+        for (j = i + 1; j < n; j++) {
+            sum += mat->data[i][j] * x->data[j][0];
+        }
 
-				return 0;
+        if (mat->data[i][i] == 0) {
+            return 1; 
+        }
+
+        x->data[i][0] = (b->data[i][0] - sum) / mat->data[i][i];
+    }
+
+    return 0;
 }
-
 
