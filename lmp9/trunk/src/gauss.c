a2464b855f1f94289c6a7d12141dc12eddfa03a7
MikolajDulny <01206761@pw.edu.pl>
2025-12-18 18:02:06 +0100

    Update gauss.c

--- lmp9/trunk/src/gauss.c
+++ lmp9/trunk/src/gauss.c
@@ -19,7 +19,7 @@ int eliminate(Matrix *mat, Matrix *b){
             double *temp = mat->data[k];
             mat->data[k] = mat->data[main_row];
             mat->data[main_row] = temp;
-            double *temp_b - b->data[k];
+            double *temp_b = b->data[k];
             b->data[k] = b->data[main_row];
             b->data[main_row] = temp_b;
             for(i = k+1; i<n; i++){
@@ -28,6 +28,7 @@ int eliminate(Matrix *mat, Matrix *b){
                             mat->data[i][j] -= factor*mat->data[k][j];}
                     b->data[i][0] -=factor*b->data[k][0];}}
     if(fabs(mat->data[n-1][n-1])<1e-12){
+            return 1;}
 
                 return 0;
 }
